e5cff3aa0e62a5a2a53f9c60b273bc973e8c7b44
HongXunPan <15889325915@163.com>
2018-08-23 17:02:13 +0800

    create relation to post&tag

--- app/PostTagRelationBlog.php
+++ app/PostTagRelationBlog.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\PostTagRelationBlog
  *
+ * @property int $id ID
  * @property int $post_id 文章ID
  * @property int $tag_id 标签ID
  * @method static \Illuminate\Database\Eloquent\Builder|\App\PostTagRelationBlog wherePostId($value)
@@ -16,13 +17,22 @@ use Illuminate\Database\Eloquent\Model;
 class PostTagRelationBlog extends Model
 {
 
-    protected $primaryKey = ['post_id', 'tag_id'];
+//    protected $primaryKey = 'id';
 
-    public $incrementing = false;
+//    public $incrementing = false;
 
     protected $table = 'post_tag_relations_blog';
 
     public $timestamps = false;
 
     //只做新增，不做查询
+    public function post()
+    {
+        return $this->hasOne('App\PostBlog', 'post_id', 'post_id');
+    }
+
+    public function tag()
+    {
+        return $this->hasOne('App\TagBlog', 'tag_id', 'tag_id');
+    }
 }
