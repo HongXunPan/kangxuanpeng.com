abbf2fec08cef204a4d8c1f5344ac01bd0de868d
HongXunPan <15889325915@163.com>
2018-08-24 16:25:57 +0800

    index add GA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,16 @@
     {{--css--}}
     <link rel="stylesheet" href="{{ asset("/css/base.css") }}" />
     <link rel="stylesheet" href="{{ asset("/css/index.css") }}" />
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-124606004-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'UA-124606004-2');
+    </script>
 </head>
 <body>
 <canvas width="3000" height="872"></canvas>
