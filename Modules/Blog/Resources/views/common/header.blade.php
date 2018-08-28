4a948de1ade9b5a934ea2a94c96d9d050c99e6e9
HongXunPan <15889325915@163.com>
2018-08-28 17:59:04 +0800

    use hieu-le/active for header

--- Modules/Blog/Resources/views/common/header.blade.php
+++ Modules/Blog/Resources/views/common/header.blade.php
@@ -4,17 +4,17 @@
         <a href="{{ url('/') }}" class="navbar-logo"> <img src="{{ asset('images/logo.png') }}" alt="HongXunPan"> </a>
         <!--nav-->
         <div class="navbar-menu">
-            <a class="current" href="{{ url('/') }}">Index</a>
+            <a class="{{ active_class(if_uri('/'), 'current') }}" href="{{ url('/') }}">Index</a>
             {{--<a href="#">Links</a>--}}
-            <a href="{{ url('about') }}">About</a>
+            <a class="{{ active_class(if_uri('about'), 'current') }}" href="{{ url('about') }}">About</a>
         </div>
         <a href="{{ url('search') }}" class="navbar-search"> <span class="icon-search"></span> </a>
         <div class="navbar-mobile-menu" onclick="">
             <span class="icon-menu cross"><span class="middle"></span></span>
             <ul>
-                <li><a class="current" href="{{ url('/') }}">Index</a></li>
+                <li><a class="{{ active_class(if_uri('/'), 'current') }}" href="{{ url('/') }}">Index</a></li>
                 {{--<li><a href="#">Links</a></li>--}}
-                <li><a href="{{ url('about') }}">About</a></li>
+                <li><a class="{{ active_class(if_uri('about'), 'current') }}" href="{{ url('about') }}">About</a></li>
             </ul>
         </div>
     </div>
