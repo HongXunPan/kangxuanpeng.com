81528607ef25e0957175462d1495a5bf5617ff70
HongXunPan <15889325915@163.com>
2018-09-29 17:11:49 +0800

    social share link & qrcode share include img from api.qrserver

--- Modules/Blog/Resources/views/post/bottomBar.blade.php
+++ Modules/Blog/Resources/views/post/bottomBar.blade.php
@@ -4,18 +4,18 @@
         <div class="bottom-bar-items social-share left">
             <span class="bottom-bar-item">Share : </span>
             <span class="bottom-bar-item bottom-bar-facebook"><a
-                        href="#"
+                        href="https://www.facebook.com/sharer/sharer.php?u={{ url()->full() }}"
                         target="_blank" title="title" rel="nofollow">facebook</a></span>
             <span class="bottom-bar-item bottom-bar-twitter"><a
-                        href="#"
+                        href="https://twitter.com/intent/tweet?url={{ url()->full() }}&text={{ $post->post_name }}"
                         target="_blank" title="title" rel="nofollow">Twitter</a></span>
             <span class="bottom-bar-item bottom-bar-weibo"><a
-                        href="#"
+                        href="http://service.weibo.com/share/share.php?url={{ url()->full() }}&title={{ $post->post_name }}"
                         target="_blank" title="title" rel="nofollow">Weibo</a></span>
             <span class="bottom-bar-item bottom-bar-qrcode"><a
-                        {{--onclick="#qrcode.toggle()"--}}
                         rel="nofollow">QRcode</a></span>
-            <div id="qrcode"></div>
+            <div id="qrcode" title="{{ url()->full() }}">
+                <img style="display: block;" src="https://api.qrserver.com/v1/create-qr-code/?size=120x120&data={{ url()->full() }}"></div>
         </div>
         <!--button nav-->
         <div class="bottom-bar-items right">
